8b716b0e387c791f9326b4308b9b69a0dedd89c3
Yuriy Tkach <yuriytkach@gmail.com>
2016-10-30 00:51:05 +0300

    Fixed test after adding throws

--- src/test/java/com/yet/spring/core/loggers/TestFileEventLogger.java
+++ src/test/java/com/yet/spring/core/loggers/TestFileEventLogger.java
@@ -17,44 +17,44 @@ import com.yet.spring.core.beans.Event;
 
 public class TestFileEventLogger {
 	
-	private File file;
+    private File file;
 
-	@Before
-	public void createFile() throws IOException {
-		this.file = File.createTempFile("test", "FileEventLogger");
-	}
-	
-	@After
-	public void removeFile() {
-		file.delete();
-	}
+    @Before
+    public void createFile() throws IOException {
+        this.file = File.createTempFile("test", "FileEventLogger");
+    }
+    
+    @After
+    public void removeFile() {
+        file.delete();
+    }
 
-	@Test
-	public void testInit() {
-		FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
-		logger.init();
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testInitFail() {
-		file.setReadOnly();
-		FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
-		logger.init();
-	}
+    @Test
+    public void testInit() throws IOException {
+        FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
+        logger.init();
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testInitFail() throws IOException {
+        file.setReadOnly();
+        FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
+        logger.init();
+    }
 
-	@Test
-	public void testLogEvent() throws IOException {
-		Event event = new Event(new Date(), DateFormat.getDateInstance());
-		FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
-		logger.init();
-		
-		String contents = FileUtils.readFileToString(this.file);
-		assertTrue(contents.isEmpty());
-		
-		logger.logEvent(event);
-		
-		contents = FileUtils.readFileToString(this.file);
-		assertFalse(contents.isEmpty());
-	}
+    @Test
+    public void testLogEvent() throws IOException {
+        Event event = new Event(new Date(), DateFormat.getDateInstance());
+        FileEventLogger logger = new FileEventLogger(file.getAbsolutePath());
+        logger.init();
+        
+        String contents = FileUtils.readFileToString(this.file);
+        assertTrue(contents.isEmpty());
+        
+        logger.logEvent(event);
+        
+        contents = FileUtils.readFileToString(this.file);
+        assertFalse(contents.isEmpty());
+    }
 
 }
