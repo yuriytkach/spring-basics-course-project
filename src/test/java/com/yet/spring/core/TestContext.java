4bfb2ca1a36aec22914972d96dbd94b54760bb7f
Yuriy Tkach <yuriytkach@gmail.com>
2016-11-03 22:15:20 +0200

    Added names to logger and showcase of EL collection projection

--- src/test/java/com/yet/spring/core/TestContext.java
+++ src/test/java/com/yet/spring/core/TestContext.java
@@ -2,11 +2,18 @@ package com.yet.spring.core;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.yet.spring.core.beans.Client;
+import com.yet.spring.core.loggers.CacheFileEventLogger;
+import com.yet.spring.core.loggers.CombinedEventLogger;
+import com.yet.spring.core.loggers.EventLogger;
+import com.yet.spring.core.loggers.FileEventLogger;
 
 public class TestContext {
 
@@ -18,6 +25,24 @@ public class TestContext {
 		ctx.close();
 		
 		assertEquals("35", client.getId());	
+	}
+	
+	@Test
+	public void testLoggersNames() {
+	    ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+	    
+	    EventLogger fileLogger = ctx.getBean("fileEventLogger", FileEventLogger.class);
+	    EventLogger cacheLogger = ctx.getBean(CacheFileEventLogger.class);
+	    CombinedEventLogger combinedLogger = ctx.getBean(CombinedEventLogger.class);
+	    
+	    assertEquals(fileLogger.getName() + " with cache", cacheLogger.getName());
+	    
+	    Collection<String> combinedNames = combinedLogger.getLoggers().stream()
+	            .map(v -> v.getName()).collect(Collectors.toList());
+	    
+	    assertEquals("Combined " + combinedNames, combinedLogger.getName());
+	    
+	    ctx.close();
 	}
 
 }
