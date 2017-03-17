4a485712602b322fa73a449094bcebf261f1d58f
Yuriy Tkach <yuriytkach@gmail.com>
2017-03-18 01:26:23 +0200

    Fixed tests. Added coverage reports

--- src/test/java/com/yet/spring/core/aspects/TestConsoleLoggerLimitAspect.java
+++ src/test/java/com/yet/spring/core/aspects/TestConsoleLoggerLimitAspect.java
@@ -1,16 +1,11 @@
 package com.yet.spring.core.aspects;
 
-import static org.mockito.Mockito.atMost;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import com.yet.spring.core.beans.Event;
+import com.yet.spring.core.loggers.EventLogger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.junit.Test;
 
-import com.yet.spring.core.beans.Event;
-import com.yet.spring.core.loggers.EventLogger;
+import static org.mockito.Mockito.*;
 
 public class TestConsoleLoggerLimitAspect {
 
@@ -19,18 +14,17 @@ public class TestConsoleLoggerLimitAspect {
         ProceedingJoinPoint jp = mock(ProceedingJoinPoint.class);
         EventLogger mockLogger = mock(EventLogger.class);
         Event mockEvent = mock(Event.class);
-        
+
         when(mockLogger.getName()).thenReturn("MockLogger");
-        
+
         ConsoleLoggerLimitAspect aspect = new ConsoleLoggerLimitAspect(2, mockLogger);
-        
+
         aspect.aroundLogEvent(jp, mockEvent);
         aspect.aroundLogEvent(jp, mockEvent);
         aspect.aroundLogEvent(jp, mockEvent);
-        
+
         verify(jp, atMost(2)).proceed();
         verify(mockLogger, atMost(1)).logEvent(mockEvent);
-        verify(mockEvent, never());
     }
 
 }
